49945d528ceae3578a239065f79e6ed477353f5b
akshada1904 <akshadajadhav2412@gmail.com>
2026-06-03 08:48:43 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,11 @@
+user="admin"
+pswd="admin@123"
+
+a=input("Enter username")
+b=input("Enter password")
+
+if a ==user and b==pswd: 
+    print("login succesfully")
+else:
+     print("Invalid credentials")
+
