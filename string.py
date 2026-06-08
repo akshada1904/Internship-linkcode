0d461b689aa5cdaafe92b8c2ae2caeeb2584968e
akshada1904 <akshadajadhav2412@gmail.com>
2026-06-08 21:01:49 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,22 @@
+#simple string print
+name = "Akshada"
+city = 'Pune'
+
+print(name)
+print(city)
+
+#length of string
+name = "Akshada"
+print(len(name))
+
+#convert to uppercase
+name = "akshada"
+print(name.upper())
+
+#convert to lowercase
+name = "AKSHADA"
+print(name.lower())
+
+#reverse string
+name = "akshada"
+print(name[::-1])
